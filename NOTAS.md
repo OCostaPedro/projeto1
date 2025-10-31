de0f6c1fa3af06fe96218bd8692620715f34fe4b
lisasalesss <115100317+lisasalesss@users.noreply.github.com>
2025-10-31 20:53:29 -0300

    Update and rename NOTAS.md to comparacoes.py

--- NOTAS.md
+++
@@ -1,1 +0,0 @@
-﻿Resumo das comparacoes
---
+++ comparacoes.py
@@ -0,0 +1,11 @@
+import os
+os.system("cls")
+
+print("A emissão anual pelo consumo de música pelo streaming Spotify é de 195 mil e 27 toneladas métricas de gás carbônico.\n")
+print("Isso equivale a:\n")
+print("A emissão de CO2 de 45 mil carros durante 1 ano\n")
+print("A emissão de CO2 de 55.260 casas durante 1 ano\n")
+print("A emissão de CO2 de 181 mil voos de São Paulo-Rio a Janeiro\n")
+
+
+print("Fonte: EPA https://www.epa.gov/energy/greenhouse-gas-equivalencies-calculator")
