95bcb42b07c1f9f14d76d7b8551448b7014af43c
Arthur Schmitz <arthurschmitz07@gmail.com>
2025-11-07 13:43:14 -0300

    Create alto.py

---
+++ alto.py
@@ -0,0 +1,3 @@
+print("Seu tempo de consumo é ALTO \n"
+      "Com um tempo acima de 30h (mais de 1800 min), sua pegada ecológica"
+      " é elevada e exige mudanças imediatas para reduzir os impactos e evitar acúmulos.")
