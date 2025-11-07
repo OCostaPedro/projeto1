cf5f784b878eaebb880fdb0ea015881dcd75365e
Arthur Schmitz <arthurschmitz07@gmail.com>
2025-11-07 17:23:33 -0300

    Refactor emission display into a function

--- comparacoes.py
+++ comparacoes.py
@@ -1,11 +1,12 @@
-import os
-os.system("cls")
+def exibir_emissao_spotify(clear=True):
+    if clear:
+        os.system('cls' if os.name == 'nt' else 'clear')
+    print("A emissão anual pelo consumo de música pelo streaming Spotify é de 195 mil e 27 toneladas métricas de gás carbônico.\n")
+    print("Isso equivale a:\n")
+    print("A emissão de CO2 de 45 mil carros durante 1 ano\n")
+    print("A emissão de CO2 de 55.260 casas durante 1 ano\n")
+    print("A emissão de CO2 de 181 mil voos de São Paulo-Rio a Janeiro\n")
+    print("Fonte: EPA https://www.epa.gov/energy/greenhouse-gas-equivalencies-calculator")
 
-print("A emissão anual pelo consumo de música pelo streaming Spotify é de 195 mil e 27 toneladas métricas de gás carbônico.\n")
-print("Isso equivale a:\n")
-print("A emissão de CO2 de 45 mil carros durante 1 ano\n")
-print("A emissão de CO2 de 55.260 casas durante 1 ano\n")
-print("A emissão de CO2 de 181 mil voos de São Paulo-Rio a Janeiro\n")
-
-
-print("Fonte: EPA https://www.epa.gov/energy/greenhouse-gas-equivalencies-calculator")
+if __name__ == "__main__":
+    exibir_emissao_spotify()
