3d7a3bc8075de7a93f432734893d1aadfc5751a9
Chris-863 <cmjm@cesar.school>
2025-11-07 14:49:27 -0300

    Update tempomedio.py

--- tempomedio.py
+++ tempomedio.py
@@ -1,3 +1,8 @@
-print("Seu tempo de consumo é MÉDIO \n"  \
+import os
+os.system("cls)
+          
+def consumo_medio():
+  print("Seu tempo de consumo é MÉDIO \n"  \
 "Com um tempo entre 16-30h(901 a 1800 min), sua pegada ecológica," \
 " não é das maiores proporções, mas ainda assim é necessário que algo seja feito para não acumular.") 
+print(consumo_medio())
