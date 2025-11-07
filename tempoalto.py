de2547749b0b2cc67225ced4eed43767f21e33b3
Chris-863 <cmjm@cesar.school>
2025-11-07 14:51:02 -0300

    Update tempoalto.py

--- tempoalto.py
+++ tempoalto.py
@@ -1,3 +1,8 @@
-print("Seu tempo de consumo é ALTO \n"
+
+import os
+os.system("cls")
+def consumo_alto():
+      print("Seu tempo de consumo é ALTO \n"
       "Com um tempo acima de 30h (mais de 1800 min), sua pegada ecológica"
       " é elevada e exige mudanças imediatas para reduzir os impactos e evitar acúmulos.")
+print(consumo_alto())
