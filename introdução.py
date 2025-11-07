a1b0fe9a144597f4053f7a08c9058e22771cc708
Chris-863 <cmjm@cesar.school>
2025-11-07 14:53:01 -0300

    Update introdução.py

--- introdução.py
+++ introdução.py
@@ -1,8 +1,10 @@
 import os
 os.system ("cls")
-print("Bem Vindo ao Make Music Green!\n" \
+def introducao():
+ print("Bem Vindo ao Make Music Green!\n" \
  \
-" Make Music Green é um WebApp inovador que transforma o modo como você se relaciona com a música e o meio ambiente. " \
-"De forma lúdica e interativa, ele ajuda você a reduzir sua pegada ecológica digital, mostrando o impacto ambiental do seu streaming musical." \
-" Ao contrário das simples calculadoras de CO₂, o Make Music Green vai além! Ele exibe indicadores reais de poluição gerada pelo uso das plataformas de streaming e sugere ações personalizadas para compensar esse impacto fazendo isso tudo dentro de uma experiência gamificada e divertida.\n" \
+ " Make Music Green é um WebApp inovador que transforma o modo como você se relaciona com a música e o meio ambiente. " \
+ "De forma lúdica e interativa, ele ajuda você a reduzir sua pegada ecológica digital, mostrando o impacto ambiental do seu streaming musical." \
+ " Ao contrário das simples calculadoras de CO₂, o Make Music Green vai além! Ele exibe indicadores reais de poluição gerada pelo uso das plataformas de streaming e sugere ações personalizadas para compensar esse impacto fazendo isso tudo dentro de uma experiência gamificada e divertida.\n" \
 "Crie uma conta e nos ajude a fazer o mundo um lugar melhor!")
+ print(introducao())
