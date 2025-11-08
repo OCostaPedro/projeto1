caf054e18aa65b1e01fd18fe9015c3b97d45a81f
OCostaPedro <pedroarthur2004@outlook.com>
2025-11-07 23:50:23 -0300

    consum baixo

--- tempomedio.py
+++ tempomedio.py
@@ -1,8 +1,8 @@
 import os
-os.system("cls)
+os.system("cls")
           
 def consumo_medio():
-  print("Seu tempo de consumo é MÉDIO \n"  \
-"Com um tempo entre 16-30h(901 a 1800 min), sua pegada ecológica," \
-" não é das maiores proporções, mas ainda assim é necessário que algo seja feito para não acumular.") 
+    print("Seu tempo de consumo é MÉDIO \n"  \
+    "Com um tempo entre 16-30h(901 a 1800 min), sua pegada ecológica," \
+    " não é das maiores proporções, mas ainda assim é necessário que algo seja feito para não acumular.") 
 print(consumo_medio())
