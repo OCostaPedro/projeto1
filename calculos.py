8e5e65c0038007a8ae43768b5646a4de9612375e
mclaralopess <mclaralopes29@gmail.com>
2025-11-08 03:46:46 -0300

    calculos.py

--- calculos.py
+++ calculos.py
@@ -1,23 +1,25 @@
 import os
+os.system("cls")
 
-def calcular_consumo_co2(minutos_musica):
-    return (27 * minutos_musica) / 1243
+media_emissao_CO2_pessoa = 27
+media_minutos_por_pessoa_brasil = 1243  
+ano = 12 
 
-def exibir_resultados(minutos_musica):
-    consumo_co2 = calcular_consumo_co2(minutos_musica)
-    print(f"Nesse ritmo, em um ano você emitirá {consumo_co2 * 12} gramas de CO².")
-    print(f"\nSe todos os usuários do Spotify Brasil seguissem o seu ritmo, seriam emitidas {consumo_co2 * 12 * 56000000} toneladas de CO² em um ano.")
-    if consumo_co2 <= 900:
-        print("Isso seria um valor menor do que o total atual.")
-    elif consumo_co2 <= 1800:
-        print("Isso seria um valor equivalente ao total atual.")
-    else:
-        print("Isso seria um valor maior do que o total atual.")
+minutos_musica = int(input("Insira quantos minutos de música você escutou no último mês: "))
 
-def main():
-    os.system("cls" if os.name == "nt" else "clear")
-    minutos_musica = int(input("Insira quantos minutos de música você escutou no último mês: "))
-    exibir_resultados(minutos_musica)
+consumo_co2 = (media_emissao_CO2_pessoa*minutos_musica)/media_minutos_por_pessoa_brasil
 
-if __name__ == "__main__":
-    main()
+if consumo_co2 <= 900:
+    print(f"Nesse ritmo, em um ano você emitirá {consumo_co2*ano} gramas de CO².")
+    print(f"\nSe todos os usuários do Spotify Brasil seguissem o seu ritmo, seriam emitidas {consumo_co2*12*56000000} toneladas de CO² em um ano.")
+    print("Isso seria um valor menor do que o total atual.")
+
+elif consumo_co2 > 900 and consumo_co2 <= 1800:
+    print(f"Nesse ritmo, em um ano você emitirá {consumo_co2*ano} gramas de CO².")
+    print(f"\nSe todos os usuários do Spotify Brasil seguissem o seu ritmo, seriam emitidas {consumo_co2*12*56000000} toneladas de CO² em um ano.")
+    print("Isso seria um valor equivalente ao total atual.")
+
+elif consumo_co2 > 1801:
+    print(f"Nesse ritmo, em um ano você emitirá {consumo_co2*ano} gramas de CO².")
+    print(f"\nSe todos os usuários do Spotify Brasil seguissem o seu ritmo, seriam emitidas {consumo_co2*12*56000000} toneladas de CO² em um ano.")
+    print("Isso seria um valor maior do que o total atual.")    
