05bc460923d1f05681d66a103f6bf2c880c8ed56
OCostaPedro <pedroarthur2004@outlook.com>
2025-11-08 00:01:42 -0300

    consumo baixo

---
+++ tempobaixo.py
@@ -0,0 +1,8 @@
+import os
+os.system("cls")
+
+def consumo_baixo():
+    print("Seu tempo de consumo é BAIXO \n"
+    "Com um tempo entre 0-30h(0 a 901 min), sua pegada ecológica,"
+    "Apesar de ser de pequena proporção, ainda é importante agir para evitar o acúmulo.") 
+print(consumo_baixo())
