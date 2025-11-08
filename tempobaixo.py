fb117b4993de1958a5ac86c207f21b63b9ab1a6e
mclaralopess <mclaralopes29@gmail.com>
2025-11-08 04:04:01 -0300

    tempobaixo.py

--- tempobaixo.py
+++ tempobaixo.py
@@ -4,5 +4,5 @@ os.system("cls")
 def consumo_baixo():
     print("Seu tempo de consumo é BAIXO \n"
     "Com um tempo entre 0-30h(0 a 901 min), sua pegada ecológica,"
-    "Apesar de ser de pequena proporção, ainda é importante agir para evitar o acúmulo.") 
+    "é de pequena proporção comparada as demais, mas ainda sim é importante agir para evitar o acúmulo.") 
 print(consumo_baixo())
